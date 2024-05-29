87c329d4f7eff2d6f7edd76bb1be1adbe37936eb
Emreyiil <126668360+Emreyiil@users.noreply.github.com>
2024-05-29 11:44:50 +0200

    left pane window

--- simpleAnalyze/screens/analyzeDataScreen.py
+++ simpleAnalyze/screens/analyzeDataScreen.py
@@ -1,15 +1,29 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QGroupBox, QListWidget, QListWidgetItem
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 from simpleAnalyze.Components.datatable import DataTable
+from simpleAnalyze.Components.leftPane import LeftPaneWidget
 
 class AnalyzeDataScreen(QWidget):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
+
+
+        main_layout = QHBoxLayout(self)
+
+
+        left_pane = LeftPaneWidget()
+        left_pane.setFixedWidth(270)
+        main_layout.addWidget(left_pane)
+
 
         self.data_table = DataTable()
-        layout.addWidget(self.data_table)
+        main_layout.addWidget(self.data_table)
 
-        self.setLayout(layout)
+        self.setLayout(main_layout)
+
 
     def display_data(self, data):
         self.data_table.update_table(data)
+
