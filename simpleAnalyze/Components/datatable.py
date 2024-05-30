dd2e002ca4336ba823a65afe0b62b2b2fe6bea10
Emreyiil <126668360+Emreyiil@users.noreply.github.com>
2024-05-30 09:41:57 +0200

    export edit column on table

--- simpleAnalyze/Components/datatable.py
+++ simpleAnalyze/Components/datatable.py
@@ -1,15 +1,13 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableView, QHeaderView, QSizePolicy, QPushButton
+from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableView, QHeaderView, QSizePolicy
 
 class DataTable(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-
         self.table_view = QTableView()
         layout.addWidget(self.table_view)
-
         self.setLayout(layout)
 
     def update_table(self, data):
@@ -19,20 +17,44 @@ class DataTable(QWidget):
         rows = data.strip().split('\n')
         headers = rows[2].split('\t')
 
+        headers.append('Edit/Export')
+
         model = QStandardItemModel()
         model.setColumnCount(len(headers))
         model.setHorizontalHeaderLabels(headers)
 
         for row_index, row in enumerate(rows[1:]):
             columns = row.split('\t')
+
+            export_icon_path = 'Export.png'
+            export_icon = QIcon(export_icon_path)
+
+            import_button = QPushButton()
+            import_button.setIcon(export_icon)
+            import_button.setFixedSize(40, 40)
+
+            import_button.setStyleSheet("background-color: red;")
+
+            print("Adding button at row", row_index)
+
             for col_index, value in enumerate(columns):
                 item = QStandardItem(value)
                 model.setItem(row_index, col_index, item)
+
+            # Add an empty item for the button column
+            model.setItem(row_index, len(headers) - 1, QStandardItem())
+            index = model.index(row_index, len(headers) - 1)
+            self.table_view.setIndexWidget(index, import_button)
+
+            widget = self.table_view.indexWidget(index)
+            print(f"Widget at index ({row_index}, {len(headers) - 1}):", widget)
 
         self.table_view.setModel(model)
         self.table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.table_view.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.table_view.resizeColumnsToContents()
+
+        self.table_view.setColumnWidth(len(headers) - 1, 100)
 
         second_row_height = 50
         self.table_view.verticalHeader().resizeSection(1, second_row_height)
