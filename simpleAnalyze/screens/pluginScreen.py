68558226224223c8206e2f26dbf3305e11e6eacf
orgu002 <122447091+orgu002@users.noreply.github.com>
2024-06-04 15:45:34 +0200

    finished merge with searchbar

--- simpleAnalyze/screens/pluginScreen.py
+++ simpleAnalyze/screens/pluginScreen.py
@@ -1,7 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox, QCheckBox, QLineEdit
+from PyQt5.QtWidgets import QHBoxLayout, QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox, QCheckBox, QLineEdit, QSpacerItem, QSizePolicy
 from PyQt5.QtCore import pyqtSignal, Qt
-import subprocess
-from simpleAnalyze.data.plugins.pluginManager import PluginManager
 
 class PluginScreen(QWidget):
     plugins_updated = pyqtSignal(list)
@@ -15,12 +13,18 @@ class PluginScreen(QWidget):
         self.search_text = ""
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-        self.search_bar = QLineEdit(self)
-        self.search_bar.setPlaceholderText("Search Plugins")
-        self.search_bar.textChanged.connect(self.filter_plugins)
-        self.layout.addWidget(self.search_bar)
+        self.setup_search_bar()
         os_type = "windows"
         self.load_plugins(os_type)
+
+    def setup_search_bar(self):
+        search_layout = QHBoxLayout()
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search Plugins")
+        search_layout.addWidget(self.search_bar)
+        search_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.layout.addLayout(search_layout)
+        self.search_bar.textChanged.connect(self.populate_plugin_checkboxes)  # Connect to the method for filtering
 
     def load_plugins(self, os_type):
         self.plugins = self.plugin_manager.get_plugins(os_type)
@@ -28,15 +32,15 @@ class PluginScreen(QWidget):
         self.populate_plugin_checkboxes()
         self.plugins_updated.emit([plugin.name for plugin in self.plugins])
 
-
-
     def populate_plugin_checkboxes(self):
-        filtered_plugins = [plugin for plugin in self.plugins if self.search_text.lower() in plugin.name.lower()]
-        for plugin in filtered_plugins:
-            checkbox = QCheckBox(plugin.name)
-            checkbox.clicked.connect(self.toggle_plugin)
-            checkbox.setChecked(True)
-            self.layout.addWidget(checkbox)
+        self.clear_layout()  # Clear existing checkboxes
+        self.search_text = self.search_bar.text()
+        for plugin in self.plugins:
+            if self.search_text.lower() in plugin.name.lower():
+                checkbox = QCheckBox(plugin.name)
+                checkbox.clicked.connect(self.toggle_plugin)
+                checkbox.setChecked(True)
+                self.layout.addWidget(checkbox)
 
     def toggle_plugin(self):
         self.selected_plugins = [checkbox.text() for checkbox in self.findChildren(QCheckBox) if checkbox.isChecked()]
