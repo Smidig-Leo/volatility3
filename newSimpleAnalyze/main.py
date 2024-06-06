af6e951219bf7701ad7d0475aecdf0eacf9ace36
Isam-M <Isammelioui@gmail.com>
2024-06-06 11:04:30 +0200

    Session data for plugins implemented

--- newSimpleAnalyze/main.py
+++ newSimpleAnalyze/main.py
@@ -1,7 +1,6 @@
 import sys
-
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from screens.mainPage import MainPage
 from screens.analyzeDataScreen import AnalyzeDataScreen
 from newSimpleAnalyze.Components.fileUploader import FileUploader
@@ -9,9 +8,7 @@ from newSimpleAnalyze.Components.chooseOs import ChooseOs
 from screens.pluginScreen import PluginScreen
 from screens.settingsPage import SettingsPage
 from PyQt5.uic import loadUi
-
-
-
+from newSimpleAnalyze.data.sessionManager import SessionManager
 
 class VolatilityApp(QMainWindow):
     activeCommandsUpdated = pyqtSignal(list)
@@ -22,17 +19,13 @@ class VolatilityApp(QMainWindow):
         loadUi('screens/ui/NavigationBar.ui', self)
         self.setWindowTitle("Volatility3")
 
-        # NavBar available buttons:
-        # self.mainBtn
-        # self.dataBtn
-        # self.pluginsBtn
-        # self.settingsBtn
+        self.session_manager = SessionManager()
 
         self.file_uploader = FileUploader()
         self.os = ChooseOs()
 
         self.main_page = MainPage(self.file_uploader, self.os)
-        self.plugins_page = PluginScreen()
+        self.plugins_page = PluginScreen(self.session_manager)
         self.data_page = AnalyzeDataScreen(self.plugins_page.activeCommandsUpdated, self.file_uploader.file_path_updated)
         self.settings_page = SettingsPage()
 
@@ -50,30 +43,37 @@ class VolatilityApp(QMainWindow):
         self.pluginsBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.plugins_page))
         self.settingsBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.settings_page))
 
+        # Set the files uploaded from previous session
+        file_paths = self.session_manager.get_file_uploaded()
+        if file_paths:
+            for file_path in file_paths:
+                self.file_uploader.add_file_path(file_path)
+
     def switch_to_data_page(self):
         self.stackedWidget.setCurrentWidget(self.data_page)
 
     def on_active_commands_updated(self, active_commands):
+        print(f"Received active commands update: {active_commands}")
         self.activeCommandsUpdated.emit(active_commands)
 
 
-    #     # Setup connections for the run analysis process
-    #     self.select_plugin.plugin_selected.connect(self.run_analysis.handle_selected_plugin)
-    #     self.run_analysis.analysis_result.connect(self.analyzed_data_screen.display_data)
-    #
-    #     # Set the files uploaded from previous session
-    #     file_paths = self.session_manager.get_file_uploaded()
-    #     if file_paths:
-    #         for file_path in file_paths:
-    #             self.select_file_screen.file_uploader.add_file_path(file_path)
-    #             self.plugin_screen.set_file_path(file_path)
-    #
-    #     # Connect the file uploader signal to update file paths in SelectDump
-    #     self.file_uploader.file_path_updated.connect(self.select_dump.update_file_paths)
+#     # Setup connections for the run analysis process
+#     self.select_plugin.plugin_selected.connect(self.run_analysis.handle_selected_plugin)
+#     self.run_analysis.analysis_result.connect(self.analyzed_data_screen.display_data)
+#
+#     # Set the files uploaded from previous session
+#     file_paths = self.session_manager.get_file_uploaded()
+#     if file_paths:
+#         for file_path in file_paths:
+#             self.select_file_screen.file_uploader.add_file_path(file_path)
+#             self.plugin_screen.set_file_path(file_path)
+#
+#     # Connect the file uploader signal to update file paths in SelectDump
+#     self.file_uploader.file_path_updated.connect(self.select_dump.update_file_paths)
 
-    # def closeEvent(self, event):
-    #     self.session_manager.save_session()
-    #     event.accept()
+# def closeEvent(self, event):
+#     self.session_manager.save_session()
+#     event.accept()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- newSimpleAnalyze/screens/pluginScreen.py
+++ newSimpleAnalyze/screens/pluginScreen.py
@@ -1,23 +1,19 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QMessageBox, QCheckBox, QMainWindow, QFrame, \
-    QHBoxLayout, QLineEdit
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QMainWindow, QFrame, QHBoxLayout, QLineEdit
+from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import Qt
 from newSimpleAnalyze.Components.py_toggle import PyToggle
 from newSimpleAnalyze.data.plugins.pluginManager import PluginManager
-import subprocess
 
 class PluginScreen(QMainWindow):
     plugins_updated = pyqtSignal(list)
     activeCommandsUpdated = pyqtSignal(list)
 
-    def __init__(self):
+    def __init__(self, session_manager):
         super().__init__()
         loadUi('screens/ui/Plugins.ui', self)
 
-        #All buttons made in code none in .ui
-
-        self.activeCommands = []
+        self.session_manager = session_manager
+        self.activeCommands = self.session_manager.get_activated_plugins()
 
         scroll_layout = QVBoxLayout()
         scroll_layout.setSpacing(0)
@@ -30,27 +26,16 @@ class PluginScreen(QMainWindow):
         self.pluginScroll.setWidget(widget)
 
         self.plugins_data = PluginManager()
-        # Connect the existing search bar from the .ui file
-        self.search_bar = self.findChild(QLineEdit,'lineEditPluginSearch')  # Ensure 'lineEditPluginSearch' matches the object name in the .ui file
+        self.search_bar = self.findChild(QLineEdit, 'lineEditPluginSearch')
         if self.search_bar is None:
             raise ValueError("Could not find the search bar widget. Please check the object name in the .ui file.")
         self.search_bar.setPlaceholderText("Search Plugins")
-        self.search_bar.textChanged.connect(lambda: self.populate_plugin_checkboxes(scroll_layout))
+        self.search_bar.textChanged.connect(lambda: self.populate_plugin_toggles(scroll_layout))
 
         self.plugins = self.plugins_data.get_plugins('windows')
-        self.populate_plugin_checkboxes(scroll_layout)
+        self.populate_plugin_toggles(scroll_layout)
 
-
-
-        for i in range(len(self.plugins_data.get_plugins('windows'))):
-            plugin = self.plugins_data.get_plugins('windows')[i]
-
-            if (i+1) % 2 == 0:
-                self.create_plugins(scroll_layout, plugin.get_name(), "secondary")
-            else:
-                self.create_plugins(scroll_layout, plugin.get_name(), "primary")
-
-    def create_plugins(self, scroll_layout, command, color):
+    def create_plugin_toggle(self, scroll_layout, plugin_name, color):
         firstFrame = QFrame()
         secondFrame = QFrame()
         thirdFrame = QFrame()
@@ -66,7 +51,6 @@ class PluginScreen(QMainWindow):
         firstFrame.setMaximumHeight(100)
 
         layout = QHBoxLayout()
-
         layout.addWidget(secondFrame)
         layout.addWidget(thirdFrame)
 
@@ -75,7 +59,7 @@ class PluginScreen(QMainWindow):
         layout2 = QVBoxLayout()
         layout3 = QVBoxLayout()
 
-        label1 = QLabel(command)
+        label1 = QLabel(plugin_name)
         label1.setStyleSheet("color:white;")
         layout2.addWidget(label1)
 
@@ -86,23 +70,23 @@ class PluginScreen(QMainWindow):
         secondFrame.setLayout(layout2)
         thirdFrame.setLayout(layout3)
 
-        toggle.stateChanged.connect(lambda: self.setActiveCommands(command, toggle.isChecked()))
+        toggle.setChecked(plugin_name in self.activeCommands)
+        toggle.stateChanged.connect(lambda: self.setActiveCommands(plugin_name, toggle.isChecked()))
 
         scroll_layout.addWidget(firstFrame)
 
-        # self.label = QLabel("Test")
-        #
-        # self.layout().addWidget(self.label)
-
-    def setActiveCommands(self, command, isChecked):
+    def setActiveCommands(self, plugin_name, isChecked):
         if isChecked:
-            self.activeCommands.append(command)
+            self.activeCommands.append(plugin_name)
         else:
-            self.activeCommands.remove(command)
+            self.activeCommands.remove(plugin_name)
+        print(f"Plugin '{plugin_name}' toggled to {'ON' if isChecked else 'OFF'}")
+        print(f"Active Commands: {self.activeCommands}")
         self.activeCommandsUpdated.emit(self.activeCommands)
+        self.plugins_updated.emit(self.activeCommands)
+        self.session_manager.set_activated_plugins(self.activeCommands)
 
-    def populate_plugin_checkboxes(self, layout):
-        # Clear current plugin checkboxes
+    def populate_plugin_toggles(self, layout):
         for i in reversed(range(layout.count())):
             widget = layout.itemAt(i).widget()
             if isinstance(widget, QFrame):
@@ -113,59 +97,4 @@ class PluginScreen(QMainWindow):
         for i, plugin in enumerate(self.plugins):
             if search_text in plugin.name.lower():
                 color = "secondary" if (i + 1) % 2 == 0 else "primary"
-                self.create_plugins(layout, plugin.name, color)
-    #     self.file_path = ""
-    #     self.selected_plugins = []
-    #     self.plugin_manager = PluginManager()
-    #     self.selected_os = "windows"
-    #
-    #     self.layout = QVBoxLayout()
-    #     self.setLayout(self.layout)
-    #
-    #     self.load_plugins(self.selected_os)
-    #
-    # def load_plugins(self, os):
-    #     self.plugin_manager.load_plugins(os, self.file_path)
-    #     self.plugins = self.plugin_manager.get_plugins()
-    #     self.populate_plugin_checkboxes()
-    #
-    # def populate_plugin_checkboxes(self):
-    #
-    #     for i in reversed(range(self.layout.count())):
-    #         widget = self.layout.itemAt(i).widget()
-    #         if widget:
-    #             widget.deleteLater()
-    #
-    #     for plugin in self.plugins:
-    #         checkbox = QCheckBox(plugin.name)
-    #         checkbox.stateChanged.connect(self.toggle_plugin)
-    #         self.layout.addWidget(checkbox)
-    #         if self.selected_os in self.selected_plugins and plugin.name in self.selected_plugins[self.selected_os]:
-    #             checkbox.setChecked(True)
-    #         self.layout.addWidget(checkbox)
-    #
-    # def set_os(self, os):
-    #     self.selected_os = str(os)
-    #     print("OS set to ", self.selected_os)
-    #     self.load_plugins(self.selected_os)
-    #
-    # def toggle_plugin(self):
-    #     plugin_name = self.sender().text()
-    #     if self.file_path:
-    #         if plugin_name in self.selected_plugins:
-    #             self.selected_plugins.remove(plugin_name)
-    #             self.sender().setStyleSheet("")
-    #         else:
-    #             self.selected_plugins.append(plugin_name)
-    #
-    #
-    #         self.plugins_updated.emit(self.selected_plugins)
-    #
-    # def set_file_path(self, file_path):
-    #     self.file_path = file_path
-    #     self.load_plugins(self.selected_os)
-    #
-    # def clear_file_path(self):
-    #     self.file_path = ""
-    #     self.selected_plugins.clear()
-    #     self.load_plugins(self.selected_os)
+                self.create_plugin_toggle(layout, plugin.name, color)
