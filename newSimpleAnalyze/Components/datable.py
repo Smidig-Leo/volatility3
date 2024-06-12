3ee52b74d9f7bb312cc9d72495fb86496ffeafb6
Emreyiil <126668360+Emreyiil@users.noreply.github.com>
2024-06-12 16:19:16 +0200

    sorting

--- newSimpleAnalyze/Components/datable.py
+++ newSimpleAnalyze/Components/datable.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtSignal, QSortFilterProxyModel, QModelIndex
 from PyQt5.QtGui import QColor, QBrush
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTableView, QSizePolicy
 
@@ -10,6 +10,23 @@ from newSimpleAnalyze.utils.numericSortFilterProxyModel import NumericSortFilter
 from newSimpleAnalyze.utils.tablePopulator import TablePopulator
 from newSimpleAnalyze.utils.tableViewConfigurator import TableViewConfigurator
 from newSimpleAnalyze.utils.cellWidgetFactory import CellWidgetFactory
+
+class NumericSortFilterProxyModel(QSortFilterProxyModel):
+    def lessThan(self, left: QModelIndex, right: QModelIndex) -> bool:
+        left_data = self.sourceModel().data(left)
+        right_data = self.sourceModel().data(right)
+
+        if left_data is None or right_data is None:
+            return False
+
+        try:
+            left_data = float(left_data)
+            right_data = float(right_data)
+        except ValueError:
+            left_data = str(left_data)
+            right_data = str(right_data)
+
+        return left_data < right_data
 
 class DataTable(QWidget):
     headers_updated = pyqtSignal(list)
